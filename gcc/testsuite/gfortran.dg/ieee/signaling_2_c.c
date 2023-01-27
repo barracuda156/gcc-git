f8404f35cebb62df84ae1bea99ebbb7ed8c498e1
Sergey Fedorov <vital.had@gmail.com>
2023-01-28 06:52:06 +0800

    libgfortran: implement fpu-ppc for macOS
    Signed-off-by: Sergey Fedorov <vital.had@gmail.com>

--- gcc/testsuite/gfortran.dg/ieee/signaling_2_c.c
+++ gcc/testsuite/gfortran.dg/ieee/signaling_2_c.c
@@ -1,3 +1,11 @@
+#ifdef __POWERPC__ // No support for issignaling in math.h on Darwin PPC
+
+int isnansf (float x)       { return __builtin_issignaling (x) ? 1 : 0; }
+int isnans  (double x)      { return __builtin_issignaling (x) ? 1 : 0; }
+int isnansl (long double x) { return __builtin_issignaling (x) ? 1 : 0; }
+
+#else
+
 #define _GNU_SOURCE
 #include <math.h>
 #include <float.h>
@@ -6,3 +14,4 @@ int isnansf (float x)       { return issignaling (x) ? 1 : 0; }
 int isnans  (double x)      { return issignaling (x) ? 1 : 0; }
 int isnansl (long double x) { return issignaling (x) ? 1 : 0; }
 
+#endif
